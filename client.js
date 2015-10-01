d0327e6d7ef3e050bd29d15a87593f16996191ca
Gogs <gogs@fake.local>
2015-10-01 08:32:21 -0500

    created client script

---
+++ client.js
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Sapient Conversations</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://bit.ly/cdn-bootstrap-css">
+	<link rel="stylesheet" href="http://bit.ly/cdn-bootstrap-theme">
+	<script src="http://bit.ly/cdn-bootstrap-jq"></script>
+
+	<script>
+		
+	    var ws = new WebSocket("ws://localhost:8000");
+	    var nickname = "sapent-bot";
+	    ws.onopen = function(e) {
+		console.log('Connection to server opened');
+	    }
+
+	    function appendLog(type, nickname, message) {
+		var messages = document.getElementById('message');
+		var messageElem = document.createElement("li");
+		var preface_label;
+		if(type === 'notification') {
+		   preface_label = "<span class=\"label label-info\">*</span>";
+		} else if(type === 'nick_update') {
+		   preface_label = "<span class=\"label label-warning\">*</span>";
+		} else {
+		   preface_label = "<span class=\label label-success\">*</span>"
+		   + nickname + "</span>";
+		}
+		var message_text = "<h2>" + preface_label + "&nbsp; &nbsp;"
+		+ message + "</h2>";
+		messageElem.innerHTML = message_text;
+		message.appendChild(messageElem);
+	    }
+
+	   ws.onmessage = function(e) {
+		var data = JSON.parse(e.data); 
+		nickname = data.nickname;
+		appendLog(data.type, data.nickname, data.message);
+		console.log("ID: [%s] = %s", data.id, data.message);
+	    }
+
+	   ws.onclose = function(e) {
+		appendLog("Connection closed");
+		console.log("Connection closed");
+	   }
+
+	   function sendMessage() {
+		var messageField = document.getElementById('message');
+		if(ws.readyState === WebSocket.OPEN) {
+			ws.send(messageField.value);
+		}
+		messageField.value = '';
+		messageField.focus();
+	   }
+
+           function disconnect() {
+		ws.close();
+	   }
+        </script>
+</head>
+
+
