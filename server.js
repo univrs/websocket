ce6a2fce59d632e36d9c5ea8bdadf3c0569eba96
Ardeshir Sepahsalar <asepahsalar@hennepintech.edu>
2016-02-14 13:32:54 -0500

    All new

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server,
-	wss = new WebSocketServer({port:8000});
+	wss = new WebSocketServer({port:8080});
 var uuid = require('node-uuid');
 
 var clients = [];
 
 function wsSend(type, client_uuid, nickname, message){
-	for( var i=0; i<clients.length; i++) {
+	for (var i=0; i<clients.length; i++) {
 		var clientSocket = clients[i].ws;
-		if(clientSocket.readyState === WebSocket.OPEN) {
-		   clientSocket.send(JSON.stringify({
+		if (clientSocket.readyState === WebSocket.OPEN) {
+		    clientSocket.send(JSON.stringify({
 			"type": type,
 			"id"  : client_uuid,
 			"nickname": nickname,
@@ -23,34 +23,40 @@ var clientIndex = 1;
 
 wss.on('connection', function(ws) {
 	var client_uuid = uuid.v4();
-	var nickname = "User: "+clientIndex;
+	var nickname = "Joymonk "+clientIndex;
 	clientIndex+=1;
 	clients.push({"id": client_uuid, "ws": ws, "nickname": nickname});
 	console.log('client [%s] connected', client_uuid);
 
 	var connect_message = nickname + " has connected";
-	wsSend("notification", client_uuid, nickname, connect_message);
+  	wsSend("notification", client_uuid, nickname, connect_message);
         
         ws.on('message', function(message) {
-		if(message.indexOf('/nick') === 0) {
-		   var nickname_array = message.split(' ');    
-		   if(nickname_array.length >=2) {
-			var old_nickname = nickname;
-			nickname = nickname_array[1];
-			var nickname_message = "Client "+old_nickname+" changed to "+nickname;
-			wsSend("nick_update", client_uuid, nickname, nickname_message);               }
-		    } else {
-			wsSend("message", client_uuid, nickname, message);
-		    }
+        	     var msg = JSON.parse(message);
+        	     
+		         if (msg.message.indexOf('/nick') === 0) {
+		           var nickname_array = msg.message.split(' '); 
+		   
+		                if (nickname_array.length >=2) {
+		                	var old_nickname = nickname;
+		             	     nickname = nickname_array[1];
+		                  	 msg.nickname_message = "Client "+old_nickname+" changed to "+nickname;
+		                	wsSend("nick_update", client_uuid, nickname, msg.nickname_message); 
+		               } 
+		         	
+		         } else {
+		            
+		            wsSend("message", client_uuid, nickname, msg.message); 
+		         }
         });
          
         var closeSocket = function(customMessage) {
-		for(var i=0; i<clients.length; i++) {
-			if(clients[i].id == client_uuid) {
+		for (var i=0; i<clients.length; i++) {
+			if (clients[i].id == client_uuid) {
 				var disconnect_message;
-				if(customMessage){
+				if (customMessage){
 					disconnect_message= customMessage;
-				}else{
+				} else {
 				  disconnect_message = nickname + " has disconnected";
 				}
 			  wsSend("notification", client_uuid, nickname, disconnect_message);
